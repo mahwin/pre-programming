bb707fb34c83976f2eef69629600c35e849986b0
youseock <mahwin7085@gmail.com>
2022-09-23 14:54:31 +0900

    Set up : front
    -nextjs ts기반으로 설치
    -css 템플릿으로 styled-components 설치

--- backend/b2c-server/src/app.module.ts
+++ backend/b2c-server/src/app.module.ts
@@ -4,13 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { VocabularyController } from './vocabulary/vocabulary.controller';
-import { VocabularyService } from './vocabulary/vocabulary.service';
-import { VocabularyModule } from './vocabulary/vocabulary.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UserModule, VocabularyModule],
-  controllers: [VocabularyController],
-  providers: [VocabularyService],
+  imports: [AuthModule, PrismaModule, UserModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
