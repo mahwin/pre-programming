c7d9b9354becd0ca88b166ecd8a3029f1928cc91
youseock <mahwin7085@gmail.com>
2022-09-24 19:33:53 +0900

    Feat : Nav에 dark 모드 버튼 추가
    - framer motion이용한 animation

--- frontend/components/Nav.tsx
+++ frontend/components/Nav.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Wapper = styled.nav`
   width: 100%;
@@ -12,6 +13,7 @@ const NavWapper = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 
   color: ${(props) => props.theme.colorTheme.fontPrimary};
   max-width: ${(props) => props.theme.windowSize.pc};
@@ -21,6 +23,7 @@ const Items = styled.ul`
   display: flex;
   align-items: center;
   padding: 8px 12px;
+  height: 100%;
 `;
 
 const Item = styled.li`
@@ -31,8 +34,44 @@ const Item = styled.li`
     background-color: ${(props) => props.theme.colorTheme.hover};
   }
 `;
+const ToggleBox = styled.div<{ isDark: boolean }>`
+  position: relative;
+  width: 150px;
+  height: 100%;
+  margin-right: 20px;
+  border-radius: 20px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: black;
+  overflow: hidden;
+  cursor: pointer;
+  justify-content: ${(props) => (props.isDark ? "end" : "start")};
+`;
+
+const Toggle = styled(motion.div)`
+  z-index: 1;
+  height: 100%;
+  width: 80px;
+  border-radius: 20px;
+  background-color: white;
+
+  opacity: 0.3;
+`;
+
+const ToggleItem = styled.p<{ isRight: boolean }>`
+  position: absolute;
+  ${(props) => (props.isRight ? "right: 20px;" : "left: 20px;")};
+  font-size: ${(props) => props.theme.fontSize.base};
+  font-weight: ${(props) => props.theme.fontWeight.bold};
+  color: white;
+`;
 
 export default function Nav() {
+  const [isDark, setIsDark] = useState<boolean>(false);
+  const onToggleClick = () => {
+    setIsDark((prev) => !prev);
+  };
   return (
     <Wapper>
       <NavWapper>
@@ -41,7 +80,13 @@ export default function Nav() {
           <Item>학습하기</Item>
           <Item>내 단어장</Item>
         </Items>
+
         <Items>
+          <ToggleBox onClick={onToggleClick} isDark={isDark}>
+            <Toggle layout />
+            <ToggleItem isRight={false}>Light</ToggleItem>
+            <ToggleItem isRight={true}>Dark</ToggleItem>
+          </ToggleBox>
           <Item>로그인</Item>
           <Item>회원가입</Item>
         </Items>
