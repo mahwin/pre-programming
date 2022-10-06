1b8cccd519fdb81697ec270ad4ae9cee2450ee0d
youseock <mahwin7085@gmail.com>
2022-10-07 00:30:16 +0900

    Style : fetch => axios로 변경

--- frontend/libs/useMutation.tsx
+++ frontend/libs/useMutation.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import axios from "axios";
 
 interface UseMutationState<T> {
   loading: boolean;
@@ -10,25 +11,24 @@ type UseMutationResult<T> = [(data: any) => void, UseMutationState<T>];
 export default function useMutation<T = any>(
   url: string
 ): UseMutationResult<T> {
-  const [state, setSate] = useState<UseMutationState<T>>({
+  const [state, setState] = useState<UseMutationState<T>>({
     loading: false,
     data: undefined,
     error: undefined,
   });
   function mutation(data: any) {
     const accessToken = localStorage.getItem("accessToken") || null;
-    fetch(`${process.env.API_HOST}${url}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${accessToken}`,
-      },
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json().catch(() => {}))
-      .then((data) => setSate((prev) => ({ ...prev, data, loading: false })))
+    axios
+      .post(`${process.env.API_HOST}/${url}`, data, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then((response) => response.data.catch(() => {}))
+      .then((data) => setState((prev) => ({ ...prev, data, loading: false })))
       .catch((error) =>
-        setSate((prev) => ({ ...prev, error, loading: false }))
+        setState((prev) => ({ ...prev, error, loading: false }))
       );
   }
   return [mutation, { ...state }];
