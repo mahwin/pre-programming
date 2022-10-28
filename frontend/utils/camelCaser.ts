f94bc732d8d1c72dbced0c9a685fda5d8eac056a
youseock <mahwin7085@gmail.com>
2022-10-28 22:01:06 +0900

    Add : util 함수 추가
    - 첫 문자가 대문자인 스트링을 camelcase로 바꾸주는 함수

--- frontend/utils/camelCaser.ts
+++ frontend/utils/camelCaser.ts
@@ -1,10 +1,13 @@
 export default function camelCaser(name: string) {
   const nameList = name.split("-");
-
   nameList.forEach((string, idx) => {
     if (idx !== 0) {
       nameList[idx] = string.slice(0, 1).toUpperCase() + string.slice(1);
     }
   });
   return nameList.join("");
+}
+
+export function camelCaserHeadLower(name: string) {
+  return name.slice(0, 1).toLowerCase() + camelCaser(name.slice(1));
 }
