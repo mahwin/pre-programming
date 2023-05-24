82afe694f449849fcc45d1acef092f611981df3a
youseock <mahwin7085@gmail.com>
2023-05-24 14:31:47 +0900

    Fix: 환경변수 설정 반영
    - 개발과 배포 환경 port 분리 내용 반영
    - 하드코딩된 port 번호를 환경변수 찾도록 변경

--- frontend/redux/vocas/vocasAPI.ts
+++ frontend/redux/vocas/vocasAPI.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 
 async function getVocas() {
   try {
-    const response = await axios.get(`${process.env.API_HOST}/vocas/all`);
+    const response = await axios.get(
+      `${process.env.API_HOST}:${process.env.PORT}/vocas/all`
+    );
     return response;
   } catch (error: any) {
     new Error(error.message);
