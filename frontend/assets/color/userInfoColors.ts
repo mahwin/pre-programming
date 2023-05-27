a47e0695c8f77c81c7a97fd02316c015e1da9593
youseock <mahwin7085@gmail.com>
2023-05-27 22:18:28 +0900

    Refactor: userInfo 컴포넌트에서만 사용되던 색상코드 assets으로 옮김
    - 옮긴 내용 반영

---
+++ frontend/assets/color/userInfoColors.ts
@@ -0,0 +1,12 @@
+const userInfoColors = {
+  bgColor: "#5a8886",
+  avatarBgColor: "#537383",
+  textColor: "#e6901a",
+  toggle: {
+    bar: "#daaa68",
+    cicle: "#e6901a",
+  },
+  svgColor: "#a29bfe",
+};
+
+export { userInfoColors };
