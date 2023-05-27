07be4f8cd7d46cc3a2e118751862ba40eb0c4903
youseock <mahwin7085@gmail.com>
2023-05-27 22:17:36 +0900

    Refactor: css, 하드 코딩된 색상 동적으로 변경
    - 필요없는 css 제거
    - 필요없는 componenet 제거
    - component 이름 직관적이게 변경
    - 하드코딩된 색상 정보 assets에 저장

--- frontend/components/UserInfo/UserInfo.tsx
+++ frontend/components/UserInfo/UserInfo.tsx
@@ -7,6 +7,7 @@ import { ChangeSvg, LoadingSvg } from "@svg";
 import dynamic from "next/dynamic";
 import Form from "./Form";
 import PageLoading from "@components/Commons/PageLoading";
+import { userInfoColors } from "../../assets/color/userInfoColors";
 
 const Avatars = dynamic(() => import("./Avatars"), {
   ssr: false,
@@ -14,19 +15,16 @@ const Avatars = dynamic(() => import("./Avatars"), {
 });
 
 const Wrapper = styled.div`
-  display: flex;
-  height: 100vh;
-  margin-top: 15vh;
-  justify-content: center;
+  width: 400px;
+  margin: 100px auto;
 `;
 
 const Row = styled.div`
   position: relative;
-  background-color: #00cec9;
-  max-width: ${(props) => props.theme.windowSize.tablet};
-  width: 400px;
   height: 500px;
+  max-width: ${(props) => props.theme.windowSize.tablet};
   border-radius: 20px;
+  background-color: ${() => userInfoColors.bgColor};
   box-shadow: rgba(249, 207, 207, 0.15) 0px 8px 16px 0px;
   overflow: hidden;
 `;
@@ -37,50 +35,38 @@ const Header = styled.div`
   width: 100%;
   justify-content: space-around;
   h1 {
-    margin-top: 24px;
-    margin-bottom: 30px;
+    margin: 24px 0 30px 0;
     font-size: 30px;
     color: white;
-    font-weight: 700;
+    font-weight: ${(props) => props.theme.fontWeight.xxbold};
   }
 `;
 
-const Cicle = styled.div`
-  z-index: 1;
+const Avartar = styled.div`
   position: absolute;
+  top: 100px;
   left: 0;
   right: 0;
-  top: 100px;
   margin: 0 auto;
   height: 100px;
   width: 100px;
   border-radius: 50px;
-  background-color: transparent;
 `;
 
 const ToggleBox = styled.div`
   width: 120px;
-  margin-top: 30px;
-  height: 100%;
+  height: 40px;
+  margin-top: 25px;
   display: flex;
   flex-direction: column;
-
   justify-content: center;
   align-items: center;
-`;
-const NormalSpan = styled.span`
-  font-weight: 900;
-  color: white;
+  position: relative;
 `;
 
-const Button = styled.button`
-  border: none;
+const NormalSpan = styled.span`
+  font-weight: ${(props) => props.theme.fontWeight.xxbold};
   color: white;
-  width: 80px;
-  height: 50px;
-  font-size: 20px;
-  border-radius: 5px;
-  background-color: #3b82f6;
 `;
 
 const Toggle = styled(motion.button)`
@@ -90,8 +76,6 @@ const Toggle = styled(motion.button)`
   padding: 0;
   width: 80px;
   height: 5px;
-  background-color: rgb(162, 155, 254);
-  /* background-color: orange; */
   display: flex;
   justify-content: flex-start;
   border-radius: 50px;
@@ -103,17 +87,17 @@ const ToggleCircle = styled(motion.div)`
   height: 20px;
   width: 20px;
   border-radius: 10px;
-  background-color: rgb(255, 255, 255);
   position: absolute;
   top: -8px;
 `;
 
 const Label = styled(motion.label)`
-  color: #a29bfe;
-  font-size: 24px;
-  font-weight: 900;
+  color: ${() => userInfoColors.textColor};
+  font-size: ${(props) => props.theme.fontSize.lg};
+  font-weight: ${(props) => props.theme.fontWeight.xxbold};
   top: -5px;
-  left: 35px;
+  left: 38px;
+
   position: absolute;
 `;
 
@@ -121,35 +105,23 @@ const SvgBox = styled(motion.div)`
   position: absolute;
   right: -28px;
   bottom: 5px;
-
   cursor: pointer;
 `;
 
 const toggleCircleVariants = {
-  left: { x: 0, backgroundColor: "rgb(162, 155, 254)" },
-  right: { x: 60, backgroundColor: "rgb(118, 111, 211)" },
+  left: { x: 0, backgroundColor: userInfoColors.toggle.bar },
+  right: { x: 60, backgroundColor: userInfoColors.toggle.cicle },
 };
 
 const toggleBarVariants = {
   left: { background: "rgb(255,255,255)", transition: { duration: 1 } },
-  right: { background: "rgb(162,155,254)", transition: { duration: 1 } },
+  right: { background: userInfoColors.toggle.bar, transition: { duration: 1 } },
 };
 
 const changeVariants = {
   visible: { opacity: 1, transition: { duration: 1 } },
   hidden: { opacity: 0, transition: { duration: 1 } },
 };
-
-const AvatarsWrapper = styled(motion.div)`
-  position: absolute;
-  left: 0;
-  right: 0;
-  margin: 0 auto;
-  height: 400px;
-  width: 600px;
-  z-index: 3;
-  background-color: skyblue;
-`;
 
 const bounceY = [-100, 20, -10, 5, -3, 0];
 
@@ -171,7 +143,6 @@ export default function UserInfo({ data }: { data: IUser }) {
   const onSelectedAvatar = (e: MouseEvent) => {
     setAvatar(e.currentTarget.id);
   };
-  console.log(data);
 
   return (
     <>
@@ -184,12 +155,15 @@ export default function UserInfo({ data }: { data: IUser }) {
               <Header>
                 <h1>Profile</h1>
                 <ToggleBox onChange={canChangeSwitch}>
-                  <div style={{ position: "relative" }}>
+                  <div>
                     <NormalSpan>CAN</NormalSpan>
-                    {!Mutatable ? (
+                    {!Mutatable && (
                       <>
                         <NormalSpan
-                          style={{ color: "#a29bfe", position: "absolute" }}
+                          style={{
+                            color: userInfoColors.textColor,
+                            position: "absolute",
+                          }}
                         >
                           &rsquo;
                         </NormalSpan>
@@ -197,10 +171,10 @@ export default function UserInfo({ data }: { data: IUser }) {
                           animate={{ y: bounceY }}
                           transition={{ duration: 2.5 }}
                         >
-                          <span>T</span>
+                          T
                         </Label>
                       </>
-                    ) : null}
+                    )}
                     <NormalSpan style={{ marginLeft: "24px" }}>
                       CHANGE
                     </NormalSpan>
@@ -220,15 +194,14 @@ export default function UserInfo({ data }: { data: IUser }) {
                   </Toggle>
                 </ToggleBox>
               </Header>
-              <Cicle>
-                {data && (
-                  <Image
-                    alt="아바타 이미지입니다."
-                    src={`/avatars/${avatar}.png`}
-                    layout="fill"
-                    priority
-                  />
-                )}
+              <Avartar>
+                <Image
+                  alt="아바타 이미지입니다."
+                  src={`/avatars/${avatar}.png`}
+                  layout="fill"
+                  priority
+                />
+
                 {Mutatable && (
                   <SvgBox
                     onClick={onClickAvatars}
@@ -236,21 +209,23 @@ export default function UserInfo({ data }: { data: IUser }) {
                     initial="hidden"
                     animate="visible"
                   >
-                    <ChangeSvg width="30" height="30" color="#a29bfe" />
+                    <ChangeSvg
+                      width="30"
+                      height="30"
+                      color={userInfoColors.svgColor}
+                    />
                   </SvgBox>
                 )}
-              </Cicle>
-              {data && (
-                <Form
-                  data={{
-                    name: data?.name,
-                    phone: data?.phone,
-                    currentAvatar: avatar,
-                  }}
-                  isAvatarChange={avatar !== data?.avatar ? true : false}
-                  isCan={Mutatable}
-                />
-              )}
+              </Avartar>
+              <Form
+                data={{
+                  name: data?.name,
+                  phone: data?.phone,
+                  currentAvatar: avatar,
+                }}
+                isAvatarChange={avatar !== data?.avatar ? true : false}
+                isCan={Mutatable}
+              />
             </Row>
           </Wrapper>
         </>
