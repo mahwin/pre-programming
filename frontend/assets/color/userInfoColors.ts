de72106b9fd5f5bbf00e642e9d97c2956b626cb6
youseock <mahwin7085@gmail.com>
2023-05-27 22:42:37 +0900

    Style: assets에 색상 추가

--- frontend/assets/color/userInfoColors.ts
+++ frontend/assets/color/userInfoColors.ts
@@ -2,11 +2,13 @@ const userInfoColors = {
   bgColor: "#5a8886",
   avatarBgColor: "#537383",
   textColor: "#e6901a",
+  svgColor: "#a29bfe",
+  inputColor: { abled: "#ced4da", disabled: "#dadcdc", text: "#212529" },
+  labelColor: "#636e72",
   toggle: {
     bar: "#daaa68",
     cicle: "#e6901a",
   },
-  svgColor: "#a29bfe",
 };
 
 export { userInfoColors };
