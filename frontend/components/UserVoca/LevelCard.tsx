aac350c204080ac0ebaf8c51cf146dfe60bb427b
youseock <mahwin7085@gmail.com>
2023-05-28 13:18:52 +0900

    Design: css, 색상 변경
    - 필요없는 css 삭제
    - 색상 assets에서 관리 하도록 옮김
    - 컴포넌트 이름 직관적으로 변경

--- frontend/components/UserVoca/LevelCard.tsx
+++ frontend/components/UserVoca/LevelCard.tsx
@@ -1,9 +1,8 @@
 import styled from "styled-components";
 import { CheckSvg } from "@svg";
+import { userVocaColors } from "assets/color/userVocaColor";
 
 const Card = styled.li`
-  position: relative;
-  margin: 0 auto;
   background-color: #fff;
   height: 80px;
   width: 125px;
@@ -16,35 +15,26 @@ const Card = styled.li`
   }
 `;
 
-const ClickedCard = styled.li`
+const ClickedCard = styled(Card)`
   position: relative;
-  margin: 0 auto;
-  background-color: #fff;
-  height: 80px;
-  width: 125px;
-  border-radius: 3px;
-  padding: 10px;
-  transition: all 0.2s ease-in-out;
-  cursor: pointer;
-  box-shadow: 0 0 0 4px rgb(0, 184, 148);
+  box-shadow: 0 0 0 4px ${userVocaColors.selectedColor};
   span {
     opacity: 1;
     transition: all 1s ease-in-out;
   }
 `;
 
-const Center = styled.div`
-  display: flex;
-  width: 100%;
-  justify-content: center;
+const Title = styled.div`
+  text-align: center;
   h3 {
-    color: #636e72;
-    font-weight: 400;
-    font-size: 16px;
+    opacity: 0.7;
+    color: ${(props) => props.theme.colorTheme.backgroundColor};
+    font-weight: ${(props) => props.theme.fontWeight.bold};
+    font-size: ${(props) => props.theme.fontSize.base};
   }
 `;
 
-const CheckBox = styled.span`
+const Check = styled.span`
   position: absolute;
   top: -10px;
   right: -20px;
@@ -61,7 +51,7 @@ const Row = styled.div`
   p {
     font-size: 18px;
     display: inline-block;
-    color: #8a81bd;
+    color: #a875d5;
     font-weight: 500;
   }
   h2 {
@@ -90,12 +80,16 @@ export default function LevelCard({
     <>
       {isClick ? (
         <ClickedCard id={category + "|" + level} onClick={onClickCard}>
-          <CheckBox>
-            <CheckSvg width="30" height="30" color="rgb(0, 184, 148)" />
-          </CheckBox>
-          <Center>
+          <Check>
+            <CheckSvg
+              width="30"
+              height="30"
+              color={userVocaColors.selectedColor}
+            />
+          </Check>
+          <Title>
             <h3>level {level}</h3>
-          </Center>
+          </Title>
           <Row>
             <p>words</p>
             <h2>{amount}</h2>
@@ -103,9 +97,9 @@ export default function LevelCard({
         </ClickedCard>
       ) : (
         <Card id={category + "|" + level} onClick={onClickCard}>
-          <Center>
+          <Title>
             <h3>level {level}</h3>
-          </Center>
+          </Title>
           <Row>
             <p>words</p>
             <h2>{amount}</h2>
