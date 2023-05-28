7baba7ec541d3cb2c5826b1b7a6eac7ff87b2293
youseock <mahwin7085@gmail.com>
2023-05-28 21:20:09 +0900

    Design: 색상 동적으로 연결
    - 점수 나타내는 svg의 색상  assets에서 받아오도록 설계
    - 하드코딩된 색상 assets에서 받아오도록 설계
    - assets에 색상 추가

--- frontend/components/UserVoca/Test.tsx
+++ frontend/components/UserVoca/Test.tsx
@@ -16,7 +16,6 @@ import { ResultCircleSvg, XMarkSvg } from "@svg";
 import Answers from "./Answers";
 import makeTestVoca from "@utils/makeTestVoca";
 import { userVocaColors } from "assets/color/userVocaColor";
-import { vocaColors } from "assets/color/vocaColors";
 
 const Overay = styled(motion.div)`
   position: fixed;
@@ -165,9 +164,6 @@ const Ul = styled.ul`
       font-weight: 700;
       margin-left: 20px;
       font-size: 24px;
-      :hover {
-        /* border: 1px solid orange; */
-      }
     }
     label:before {
       content: "";
@@ -308,7 +304,6 @@ const ButtonBox = styled.div`
   justify-content: center;
   gap: 10px;
   height: 50px;
-  border: 1px solid;
 `;
 
 const ResultBtn = styled(Button)`
@@ -427,20 +422,34 @@ export default function Quiz({ vocas, howMany, handleClickTest }: IQuiz) {
             ) : (
               <>
                 <Title>Result Summary</Title>
-                <div style={{ border: "1px solid #325b79" }} />
+                <div
+                  style={{
+                    border: `1px solid ${userVocaColors.test.progressBarColor}`,
+                  }}
+                />
                 <Row>
                   <ColorBox>
-                    <Color style={{ backgroundColor: "#ac4e6b" }}></Color>
+                    <Color
+                      style={{
+                        backgroundColor: userVocaColors.test.correctColor,
+                      }}
+                    ></Color>
                     <p>Correct : {`${correctNum}`}</p>
                   </ColorBox>
                   <ColorBox>
-                    <Color style={{ backgroundColor: "#977f89" }}></Color>
+                    <Color
+                      style={{
+                        backgroundColor: userVocaColors.test.inCorrectColor,
+                      }}
+                    ></Color>
                     <p>InCorrect : {`${maxNum - correctNum}`}</p>
                   </ColorBox>
                 </Row>
                 <div>
                   <ResultCircleSvg
                     percent={((correctNum * 100) / maxNum).toFixed(2) + ""}
+                    correctColor={userVocaColors.test.correctColor}
+                    inCorrectColor={userVocaColors.test.inCorrectColor}
                   />
                 </div>
                 <ButtonBox>
