e62467242ac6bf79e6fb952300dbf5461054f282
youseock <mahwin7085@gmail.com>
2023-05-29 17:39:29 +0900

    Design: css 수정, componenet 네이밍 변경, 색상 assets에서 관리
    - 필요 없거나 중복된 css 제거
    - componenet 네이밍 직관적으로 변경
    - 색상 assets에서 관리

--- frontend/assets/color/vocaColors.ts
+++ frontend/assets/color/vocaColors.ts
@@ -6,6 +6,14 @@ const vocaColors = {
     xBtnColor: "#a9a9a9",
     cardTextColor: "#8a81bd",
   },
+  vocaDetail: {
+    cardBgColor: "#485460",
+    cardBoarderColor: "#dfe6e9",
+    checkboxBgColor: "#dfe6e9",
+    checkColor: "#485460",
+    titleColor: "#dfe6e9",
+    modalBtnColor: "#45b9a5",
+  },
 };
 
 export { vocaColors };
