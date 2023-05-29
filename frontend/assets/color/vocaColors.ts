b2ceca05c691b4c43fc172d1121ef8d0431ac550
youseock <mahwin7085@gmail.com>
2023-05-29 17:00:56 +0900

    Design: css 개선, 색상 assets에서 관리, componenet 네이밍 변경
    - 필요없거나 중복된 css 수정
    - 과하게 중첩된 component 분리
    - 색상 assets에서 관리
    - componenet 네이밍 직관적이게 변경

---
+++ frontend/assets/color/vocaColors.ts
@@ -0,0 +1,11 @@
+const vocaColors = {
+  addVoca: {
+    btnColor: "#dfe6e9",
+    btnBgColor: "#949fb0",
+    cardsBgColor: "#333a45",
+    xBtnColor: "#a9a9a9",
+    cardTextColor: "#8a81bd",
+  },
+};
+
+export { vocaColors };
