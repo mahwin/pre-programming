204b4e415ba71eb2180b01ddca0265c8c8ca1067
youseock <mahwin7085@gmail.com>
2023-05-29 17:00:56 +0900

    Design: css 개선, 색상 assets에서 관리, componenet 네이밍 변경
    - 필요없거나 중복된 css 수정
    - 과하게 중첩된 component 분리
    - 색상 assets에서 관리
    - componenet 네이밍 직관적이게 변경

--- frontend/components/Vocas/AddVoca.tsx
+++ frontend/components/Vocas/AddVoca.tsx
@@ -6,12 +6,12 @@ import { useSelector } from "react-redux";
 import { useRouter } from "next/router";
 import { IState } from "../../redux/initialState";
 import useMutation from "@utils/useMutation";
+import { vocaColors } from "assets/color/vocaColors";
 
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
   margin-top: 40px;
-  gap: 30px;
 `;
 
 const ButtonBox = styled.div`
@@ -25,30 +25,28 @@ const Button = styled(motion.button)`
   height: 50px;
   width: 150px;
   border: none;
-  color: #dfe6e9;
+  border-radius: 5px;
+  color: ${vocaColors.addVoca.btnColor};
   font-size: 20px;
-  background-color: #949fb0;
+  background-color: ${vocaColors.addVoca.btnBgColor};
   display: flex;
   justify-content: center;
   align-items: center;
+  gap: 10px;
   :hover {
     background-color: ${(props) => props.theme.colorTheme.hoverPrimary};
     transition: all 0.2s ease-in-out;
     transform: scale(1.05);
   }
-  p {
-    margin-right: 10px;
-  }
 `;
+
 const Cards = styled(motion.ul)`
-  position: relative;
   margin-top: 40px;
-  width: 100%;
-  background-color: #333a45;
   padding: 24px;
   display: grid;
-  gap: 24px;
   grid-template-columns: repeat(3, 1fr);
+  gap: 24px;
+  background-color: ${vocaColors.addVoca.cardsBgColor};
 `;
 
 const Card = styled(motion.li)`
@@ -70,12 +68,12 @@ const Arrow = styled(motion.div)`
   right: 230px;
   width: 0;
   height: 0;
-  border-bottom: 20px solid #333a45;
+  border-bottom: 20px solid ${vocaColors.addVoca.cardsBgColor};
   border-left: 20px solid transparent;
   border-right: 20px solid transparent;
 `;
 
-const CancleBtnBox = styled.button`
+const CancleBtnBox = styled.div`
   position: absolute;
   height: 30px;
   width: 30px;
@@ -83,63 +81,59 @@ const CancleBtnBox = styled.button`
   right: 5px;
   display: flex;
   justify-content: center;
-  cursor: pointer;
   align-items: center;
+  cursor: pointer;
   border: none;
-  background-color: transparent;
   :hover {
     transform: scale(1.2);
-    div {
-      background-color: darkgray;
-    }
   }
 `;
 
-const Bar = styled.div`
+const XBar = styled.div`
   position: absolute;
   border: none;
   width: 15px;
   height: 3px;
   transform: rotate(45deg);
-  background-color: black;
+  background-color: ${vocaColors.addVoca.xBtnColor};
   &:nth-child(2) {
     transform: rotate(135deg);
   }
 `;
 
+const Row = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+`;
+
 const CardContents = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 12px;
   h3 {
     padding: 5px;
-    font-size: 16px;
-    font-weight: 600;
-    margin-top: 8px;
-    text-align: center;
+    font-size: ${(props) => props.theme.fontSize.base};
+    font-weight: ${(props) => props.theme.fontWeight.bold};
     color: black;
   }
   p {
-    text-align: center;
     color: #8a81bd;
-    font-weight: 600;
-    margin-top: 10px;
+    font-weight: ${(props) => props.theme.fontWeight.xbold};
   }
   h2 {
+    margin-top: 5px;
     text-align: center;
     font-weight: 600;
-    margin-top: 10px;
-    font-size: 36px;
-    p {
-      display: block;
-      height: 100%;
-      margin-top: 24px;
-      color: black;
-      font-size: 14px;
-    }
+    font-size: ${(props) => props.theme.fontSize.lg};
   }
-`;
-
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
+  h2 p {
+    margin-top: 10px;
+    color: black;
+    font-size: 14px;
+  }
 `;
 
 const CardContentBox = styled.div``;
@@ -272,12 +266,12 @@ export default function AddVaca({
                 voca && (
                   <Card variants={cardVariants} key={idx + ""}>
                     <CancleBtnBox onClick={onClickCheck} id={idx + ""}>
-                      <Bar />
-                      <Bar />
+                      <XBar />
+                      <XBar />
                     </CancleBtnBox>
                     <CardContents>
                       <h3>Level {idx}</h3>
-                      <Grid>
+                      <Row>
                         <CardContentBox>
                           <p>
                             <small>
@@ -299,7 +293,7 @@ export default function AddVaca({
                             <p>{cardData?.[idx - 1].frequency} 이상</p>
                           </h2>
                         </CardContentBox>
-                      </Grid>
+                      </Row>
                     </CardContents>
                   </Card>
                 )
