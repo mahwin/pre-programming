7ace4119686da014ec2182793591b63322fc3ba0
youseock <mahwin7085@gmail.com>
2023-05-30 18:41:05 +0900

    Refactor: 코드 개선
    - data를 복사하고 data를 앞에서 size씩 자르던 코드에서 idx를 data를 size크기 씩 slice하는 코드로 변경

--- frontend/utils/chunk.ts
+++ frontend/utils/chunk.ts
@@ -6,11 +6,12 @@ interface IProps {
 }
 
 export default function chunk(data: IProps[], size: number) {
-  const items = [...data];
-  const arr = [];
-  while (items.length) {
-    arr.push(items.splice(0, size));
+  let preIdx = 0;
+  let result = [];
+  const maxLen = Math.ceil(data.length / size);
+  for (let i = 0; i < maxLen; i++) {
+    result.push(data.slice(preIdx, preIdx + size));
+    preIdx += size;
   }
-
-  return arr;
+  return result;
 }
