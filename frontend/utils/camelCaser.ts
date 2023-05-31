951e4d4236a5027ef98027cd5a0928d07f1de692
youseock <mahwin7085@gmail.com>
2023-05-31 17:39:00 +0900

    Refactor: 함수 범용성 있게 변경
    - -으로 연결된 string camelCase 형식으로 변경하는 함수
    - 좀 더 범용성 있게 변경
    - 네이밍 직관적으로 변경

--- frontend/utils/camelCaser.ts
+++ frontend/utils/camelCaser.ts
@@ -2,12 +2,13 @@ export default function formatter(name: string) {
   const nameList = name.split("-");
   nameList.forEach((string, idx) => {
     if (idx !== 0) {
-      nameList[idx] = string[1].toUpperCase() + string.slice(1);
+      nameList[idx] = string[0].toUpperCase() + string.slice(1);
     }
   });
-  return nameList.join("");
+
+  return camelCaser(nameList.join(""));
 }
 
 export function camelCaser(name: string) {
-  return name[0].toLowerCase() + formatter(name.slice(1));
+  return name[0].toLowerCase() + name.slice(1);
 }
