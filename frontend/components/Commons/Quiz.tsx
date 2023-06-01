31fb0b7f3b495bd5a7a89235037eac7611634721
youseock <mahwin7085@gmail.com>
2023-06-01 17:20:05 +0900

    Refactor: quiz 문항 컴포넌트 분리

--- frontend/components/Commons/Quiz.tsx
+++ frontend/components/Commons/Quiz.tsx
@@ -17,6 +17,7 @@ import QuizResult from "../Commons/QuizResult";
 import getQuiz from "@utils/makeQuiz";
 
 import { quizColors } from "assets/color/quizColors";
+import CheckList from "./CheckList";
 
 const Overay = styled(motion.div)`
   position: fixed;
@@ -96,170 +97,6 @@ const ProgressCicle = styled.div`
   border: 3px solid ${quizColors.quiz.lableColor};
   background-color: ${quizColors.quiz.bgColor};
   transition: all 1s ease-in-out;
-`;
-
-const Ul = styled.ul`
-  margin: auto;
-  margin-top: 10px;
-  text-align: left;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  list-style-type: none;
-  cursor: pointer;
-  li {
-    width: 400px;
-    position: relative;
-    padding: 10px 10px 10px 60px;
-    display: flex;
-    input:checked ~ div {
-      opacity: 1;
-      animation-name: ${explode};
-      animation-duration: 0.35s;
-      div:nth-child(1) {
-        animation-name: ${dropZero};
-        animation-delay: 0.1s;
-        animation-duration: 0.9s;
-        animation-fill-mode: forwards;
-      }
-      div:nth-child(2) {
-        animation-name: ${dropOne};
-        animation-delay: 0.1s;
-        animation-duration: 0.9s;
-        animation-fill-mode: forwards;
-      }
-      div:nth-child(3) {
-        animation-name: ${dropTwo};
-        animation-delay: 0.1s;
-        animation-duration: 0.9s;
-        animation-fill-mode: forwards;
-      }
-      div:nth-child(4) {
-        animation-name: ${dropThree};
-        animation-delay: 0.1s;
-        animation-duration: 0.9s;
-        animation-fill-mode: forwards;
-      }
-      div:nth-child(5) {
-        animation-name: ${dropFour};
-        animation-delay: 0.1s;
-        animation-duration: 0.9s;
-        animation-fill-mode: forwards;
-      }
-      div:nth-child(6) {
-        animation-name: ${dropFive};
-        animation-delay: 0.1s;
-        animation-duration: 0.9s;
-        animation-fill-mode: forwards;
-      }
-      div:nth-child(7) {
-        animation-name: ${dropSix};
-        animation-delay: 0.1s;
-        animation-duration: 0.9s;
-        animation-fill-mode: forwards;
-      }
-      div:nth-child(8) {
-        animation-name: ${dropSeven};
-        animation-delay: 0.1s;
-        animation-duration: 0.9s;
-        animation-fill-mode: forwards;
-      }
-    }
-    label {
-      cursor: pointer;
-      color: ${quizColors.quiz.lableColor};
-      font-weight: 700;
-      margin-left: 20px;
-      font-size: 24px;
-    }
-    label:before {
-      content: "";
-      width: 10px;
-      height: 10px;
-      background: #fff;
-      position: absolute;
-      left: 2.5px;
-      top: 17.5px;
-      box-sizing: border-box;
-      border-radius: 50%;
-    }
-  }
-`;
-
-const Bullet = styled.div`
-  width: 30px;
-  height: 30px;
-  position: absolute;
-  left: calc(-15px / 2);
-  top: calc(15px / 2);
-  border: 5px solid #fff;
-  opacity: 0;
-  border-radius: 50%;
-`;
-
-const Line = styled.div`
-  position: absolute;
-  width: 10px;
-  height: 2px;
-  border: 1px solid ${quizColors.quiz.selectedColor};
-  background-color: #fff;
-  opacity: 1;
-`;
-const BulletLineZero = styled(Line)`
-  left: 11px;
-  top: -21px;
-  transform: translateY(9px);
-  width: 2px;
-  height: 10px;
-`;
-const BulletLineOne = styled(Line)`
-  right: -12px;
-  top: -10px;
-  transform: rotate(-45deg) translate(-9px);
-`;
-const BulletLineTwo = styled(Line)`
-  right: -20px;
-  top: 11px;
-  transform: translate(-9px);
-`;
-const BulletLineThree = styled(Line)`
-  right: -12px;
-  top: 32px;
-  transform: rotate(55deg) translate(-9px);
-`;
-const BulletLineFour = styled(Line)`
-  left: -10px;
-  top: -11px;
-  transform: rotate(55deg) translate(9px);
-`;
-const BulletLineFive = styled(Line)`
-  left: -20px;
-  top: 11px;
-  transform: translate(9px);
-`;
-const BulletLineSix = styled(Line)`
-  left: -8px;
-  top: 33px;
-  transform: rotate(-55deg) translate(9px);
-`;
-
-const BulletLineSeven = styled(Line)`
-  width: 2px;
-  height: 10px;
-  left: 11px;
-  bottom: -22px;
-  transform: translateY(-9px);
-`;
-const Input = styled.input.attrs({ type: "radio", name: "answer" })`
-  opacity: 0;
-  z-index: 1;
-  width: 100%;
-  height: 100%;
-  margin: 0;
-  padding: 0;
-  position: absolute;
-  left: 0;
-  cursor: pointer;
 `;
 
 const Button = styled.button`
@@ -465,7 +302,7 @@ export default function Quiz({ vocas, howMany, handleClickTest }: IQuiz) {
           </>
         ) : (
           <>
-            <Title>Quiz {`${currentStep + 1} / ${maxNum}`}</Title>
+            <Title>Quiz {`${currentStep} / ${maxNum}`}</Title>
             <ProgressBarBox>
               <Progress value={currentStep} max={maxNum} />
               {[...Array(maxNum + 1)].map((v, i) => (
@@ -485,84 +322,12 @@ export default function Quiz({ vocas, howMany, handleClickTest }: IQuiz) {
                   다음 중 {testData?.[currentStep].question}의 의미로 올바른
                   것은 ?
                 </h2>
-                <Ul>
-                  <li>
-                    <Input
-                      onChange={onChangeInput}
-                      id="1"
-                      checked={answer[1]}
-                    />
-                    <label>{testData?.[currentStep].selectList[0]}</label>
-                    <Bullet>
-                      <BulletLineZero />
-                      <BulletLineOne />
-                      <BulletLineTwo />
-                      <BulletLineThree />
-                      <BulletLineFour />
-                      <BulletLineFive />
-                      <BulletLineSix />
-                      <BulletLineSeven />
-                    </Bullet>
-                  </li>
-                  <li>
-                    <label htmlFor="2">
-                      {testData?.[currentStep].selectList[1]}
-                    </label>
-                    <Input
-                      onChange={onChangeInput}
-                      id="2"
-                      checked={answer[2]}
-                    />
-                    <Bullet>
-                      <BulletLineZero />
-                      <BulletLineOne />
-                      <BulletLineTwo />
-                      <BulletLineThree />
-                      <BulletLineFour />
-                      <BulletLineFive />
-                      <BulletLineSix />
-                      <BulletLineSeven />
-                    </Bullet>
-                  </li>
-                  <li>
-                    <Input
-                      onChange={onChangeInput}
-                      id="3"
-                      checked={answer[3]}
-                    />
-                    <label>{testData?.[currentStep].selectList[2]}</label>
-                    <Bullet>
-                      <BulletLineZero />
-                      <BulletLineOne />
-                      <BulletLineTwo />
-                      <BulletLineThree />
-                      <BulletLineFour />
-                      <BulletLineFive />
-                      <BulletLineSix />
-                      <BulletLineSeven />
-                    </Bullet>
-                  </li>
-                  <li>
-                    <Input
-                      onChange={onChangeInput}
-                      id="4"
-                      checked={answer[4]}
-                    />
-                    <label htmlFor="4">
-                      {testData?.[currentStep].selectList[3]}
-                    </label>
-                    <Bullet>
-                      <BulletLineZero />
-                      <BulletLineOne />
-                      <BulletLineTwo />
-                      <BulletLineThree />
-                      <BulletLineFour />
-                      <BulletLineFive />
-                      <BulletLineSix />
-                      <BulletLineSeven />
-                    </Bullet>
-                  </li>
-                </Ul>
+                <CheckList
+                  onChangeInput={onChangeInput}
+                  currentStep={currentStep}
+                  testData={testData}
+                  answer={answer}
+                ></CheckList>
                 {answer.includes(true) ? (
                   <>
                     {answerList.length === maxNum - 1 ? (
