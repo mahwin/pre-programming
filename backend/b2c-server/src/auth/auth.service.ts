8fb114c3f12894ffce1fbd2bbfb1559c509362b6
youseock <mahwin7085@gmail.com>
2023-06-04 21:50:57 +0900

    Feat: 공용 아이디 사용에 대한 api 기능 추가
    - phone 01012341234, token 000000로 접근하려는 유저에 대해 공용 아이디에 대한 인증 권한 부여

--- backend/b2c-server/src/auth/auth.service.ts
+++ backend/b2c-server/src/auth/auth.service.ts
@@ -14,12 +14,12 @@ export class AuthService {
     private configService: ConfigService,
   ) {}
   async signIn({ phone }: LoginDto) {
-    // 개발모드면 twillio 사용하지 않고 토큰만 발행
-    if (process.env.NODE_ENV === 'development')
-      ({
+    // 개발모드거나 공용 아이디면 twillio 사용하지 않고 통과 토큰 전송
+    if (process.env.NODE_ENV === 'development' || phone === '01012341234') {
+      return {
         ok: true,
-        payload: '529880',
-      });
+      };
+    }
 
     const twilioClient = twilio(
       this.configService.get('TWILIO_SID'),
@@ -27,9 +27,9 @@ export class AuthService {
     );
 
     const payload = Math.floor(100000 + Math.random() * 900000) + '';
-    //토큰이랑 user id를 연결 시킬껀데
-    //만약 가입한 적이 있으면 그 id와 token을 같이 저장하고 아니라면
-    //유저를 생성하고 생성된 유저의 id와 token을 같이 저장한다.
+    // 토큰이랑 user id를 연결 시킬껀데
+    // 만약 가입한 적이 있으면 그 id와 token을 같이 저장하고 아니라면
+    // 유저를 생성하고 생성된 유저의 id와 token을 같이 저장한다.
     const token = await this.prisma.token.create({
       data: {
         payload,
@@ -51,15 +51,18 @@ export class AuthService {
     const message = await twilioClient.messages.create({
       messagingServiceSid: this.configService.get('TWILIO_MSID'),
       to: `82${phone.slice(1)}`,
-      body: `Your login token is ${payload}.`,
+      body: `Wellcome! we are pre-programmers.               Your login token is ${payload}.`,
     });
     return {
       ok: true,
-      payload,
     };
   }
 
   async confirm({ token }) {
+    // 000000 토큰은 검증안 하고 공용 아이디로 로그인 허락
+    if (token == '000000')
+      return { ok: true, accessToken: this.jwtService.sign({ userId: 1 }) };
+
     const foundToken = await this.prisma.token.findUnique({
       where: {
         payload: token,
