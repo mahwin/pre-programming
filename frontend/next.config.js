430fd8a6097ca17530d311225478926f4be75a0d
youseock <mahwin7085@gmail.com>
2023-06-05 21:12:46 +0900

    Build: 빌드 포트, api host 정리
    - 빌드 포트 3000번으로 변경
    - API_HOST => DEPLOY_API_HOST으로 좀 더 정확하게 명명

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,11 +9,11 @@ const nextConfig = {
     API_HOST:
       process.env.NODE_ENV === "development"
         ? process.env.DEV_API_HOST
-        : process.env.API_HOST,
+        : process.env.DEPLOY_API_HOST,
     PORT:
       process.env.NODE_ENV === "development"
         ? process.env.DEV_PORT || 3306
-        : process.env.DEPLOY_PORT || 3306,
+        : process.env.DEPLOY_PORT || 443,
   },
 };
 
