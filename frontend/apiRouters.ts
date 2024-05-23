d6fd44370bbba0d76f7f15ffcd272910ce5f4cd0
youseock <mahwin7085@gmail.com>
2024-05-23 23:51:58 +0900

    refactor: router 관련 path 한 곳에서 관리

--- frontend/apiRouters.ts
+++ frontend/apiRouters.ts
@@ -1,5 +1,12 @@
-export const apiRoutes = {};
+export const apiRoutes = {
+  logout: "/auth/signout",
+};
 
 export const pageRoutes = {
   main: "/",
-};
+  signIn: "/signIn",
+  me: "/me",
+  myVocabulary: "/me/vocabulary",
+} as const;
+
+export type RouteValue = (typeof pageRoutes)[keyof typeof pageRoutes];
