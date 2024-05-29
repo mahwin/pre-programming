a8bc127fc005aa25db2731c897cee0f4c0200e61
youseock <mahwin7085@gmail.com>
2024-05-29 21:42:21 +0900

    feat: 쓰로틀링이 적용된 axios 인스턴스 생성

--- frontend/api/index.ts
+++ frontend/api/index.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosError } from "axios";
+import { throttlingApi } from "./throttlingApi";
 import { authManager } from "@modules/Auth";
 import { isNil } from "@utils/typeGuard";
 import { apiRoutes } from "../apiRouters";
@@ -15,23 +16,23 @@ const api = axios.create({ ...baseApiOption });
 
 const authApi = axios.create({ ...baseApiOption });
 
-// 요청 인터셉터
 authApi.interceptors.request.use(async (config) => {
   const token = authManager.get();
 
   if (isNil(token)) await genenateAccessToken();
 
   config.headers.Authorization = `Bearer ${authManager.get()}`;
+
   return config;
 });
 
 async function genenateAccessToken() {
   try {
-    const { data } = await api.get(apiRoutes.generateAccessToken);
+    const { data } = await throttlingApi.get(apiRoutes.generateAccessToken);
     authManager.set(data.accessToken);
   } catch (error) {
     throw new Error("refresh 토큰 검증 실패");
   }
 }
 
-export { api, authApi, AxiosError as ApiError };
+export { api, authApi, baseApiOption, AxiosError as ApiError };
---
+++ frontend/api/throttlingApi.ts
@@ -0,0 +1,30 @@
+import axios, { AxiosResponse } from "axios";
+import { baseApiOption } from "./index";
+
+const throttlingApi = axios.create({ ...baseApiOption });
+
+const requestSet = new Set();
+
+const createRequestKey = (...arr: unknown[]) =>
+  arr.reduce((acc, cur) => `${acc}&${cur}`);
+
+throttlingApi.interceptors.request.use((config) => {
+  const key = createRequestKey(config.url, config.method);
+
+  if (requestSet.has(key)) {
+    axios.CancelToken.source().cancel("중복 api 요청을 무시합니다.");
+  } else {
+    requestSet.add(key);
+  }
+
+  return config;
+});
+
+throttlingApi.interceptors.response.use((response: AxiosResponse) => {
+  const { url, method } = response.config;
+  const key = createRequestKey(url, method);
+  requestSet.delete(key);
+  return response;
+});
+
+export { throttlingApi };
